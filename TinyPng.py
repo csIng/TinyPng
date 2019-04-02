9ac65c9ca5b1172254aa8034da5d4700eb1f40b3
csIng <cuisuai@gmail.com>
2019-04-02 12:31:58 +0800

    check input params

--- TinyPng.py
+++ TinyPng.py
@@ -6,35 +6,65 @@ import os
 import tinify
 
 
-def tiny(argv):
+class Params(object):
     server_key = ''
-    in_file = ''
-    out_file = ''
-    try:
-        opts, args = getopt.getopt(argv, 'hk:i:o:', ["k=", "ifile=", "ofile="])
-    except getopt.GetoptError:
-        print_help()
-    for opt, arg in opts:
-        if opt == '-h':
-            print_help()
-        elif opt in ("-k", "--k"):
-            server_key = arg
-        elif opt in ("-i", "--ifile"):
-            in_file = arg
-        elif opt in ("-o", "-ofile"):
-            out_file = arg
-    if not server_key:
+    input_file = ''
+    output_file = ''
+
+
+def is_invalid(params):
+    return not params or not params.server_key or not params.input_file
+
+
+def get_files(params):
+    input_files = []
+    output_files = []
+    if os.path.isfile(params.input_file):
+        input_files.append(params.input_file)
+        output_files.append(params.output_file)
+
+    elif os.path.isdir(params.input_file) and not os.path.exists(params.output_file):
+        os.mkdir(params.output_file)
+
+    if os.path.isdir(params.input_file) and os.path.isdir(params.output_file):
+        in_files = os.listdir(params.input_file)
+        for file in in_files:
+            input_files.append(os.path.join(params.input_file, file))
+            output_files.append(os.path.join(params.output_file, file))
+    return input_files, output_files
+
+
+def tiny(argv):
+    p = parse_params(argv)
+    if is_invalid(p):
+        print('input file not exists')
         print_help()
 
     print("start")
-    if os.path.isfile(in_file) and os.path.isfile(out_file):
-        tiny_file(server_key, in_file, out_file)
-    elif os.path.isdir(in_file) and os.path.isdir(out_file):
-        in_files = os.listdir(in_file)
-        for file in in_files:
-            tiny_file(server_key, os.path.join(in_file, file), os.path.join(out_file, file))
-
+    input_files, output_files = get_files(p)
+    if not input_files or not output_files:
+        print_help()
+    for i, input_file in enumerate(input_files):
+        tiny_file(p.server_key, input_file, output_files[i])
     print("end")
+
+
+def parse_params(argv):
+    result = Params
+    try:
+        opts, args = getopt.getopt(argv, 'hk:i:o:', ["k=", "ifile=", "ofile="])
+        for opt, arg in opts:
+            if opt == '-h':
+                print_help()
+            elif opt in ("-k", "--k"):
+                result.server_key = arg
+            elif opt in ("-i", "--ifile"):
+                result.input_file = arg
+            elif opt in ("-o", "-ofile"):
+                result.output_file = arg
+    except getopt.GetoptError:
+        pass
+    return result
 
 
 def tiny_file(server_key, in_file, out_file):
